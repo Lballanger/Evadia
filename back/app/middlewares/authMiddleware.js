5378c281172f2a41dc5a516a6f37df8b1d8c29c3
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-22 00:16:05 +0200

    ✨ Create authMiddleware

---
+++ back/app/middlewares/authMiddleware.js
@@ -0,0 +1,33 @@
+const jwtService = require('../services/jwtService');
+
+// eslint-disable-next-line consistent-return
+exports.authMiddleware = async (req, res, next) => {
+  try {
+    // We get the authorization from request headers
+    const { authorization } = req.headers;
+
+    // If Authorization have no value, return error message with status 401
+    if (!authorization)
+      return res.status(401).json({ error: 'No token found' });
+
+    // Extract token from authorization value ('Bearer the-token-here')
+    const token = authorization.split(' ')[1];
+
+    // If no token found, return error message with status 401
+    if (token == null) return res.status(401).json({ error: 'No token found' });
+
+    // Decode the token
+    const decoded = await jwtService.validateToken(token);
+
+    // If decode is null, return error message with status 401
+    if (decoded == null)
+      return res.status(401).json({ error: 'Invalid or expired token' });
+
+    // If token is valid, we set the user data to the request
+    req.user = decoded;
+
+    next();
+  } catch (err) {
+    res.status(403).json({ error: 'You must be authenticated' });
+  }
+};
