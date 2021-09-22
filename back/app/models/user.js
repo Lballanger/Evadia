d3fd3ee3681b30241c34bd47726a5f818942f679
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-22 14:23:12 +0200

    📝 Add documentation in User model

--- back/app/models/user.js
+++ back/app/models/user.js
@@ -1,5 +1,15 @@
 const client = require('../database');
 
+/**
+ * An entity representing an identified user
+ * @typedef User
+ * @property {number} id
+ * @property {string} firstname
+ * @property {string} lastname
+ * @property {string} email
+ * @property {string} password
+ * @property {string} role
+ */
 class User {
   /**
    * The User class
@@ -12,9 +22,17 @@ class User {
     }
   }
 
+  /**
+   * Function that return a user from the database if found or null if not
+   * @param {number} id the id of the user to find
+   * @async
+   * @static
+   * @returns {User<object>} new instance of user or null
+   * @throws {Error} if the id didn't match any user in the database
+   */
   static async getById(id) {
     try {
-      const result = await client.query('SELECT * FROM users WHERE id = $1', [
+      const result = await client.query('SELECT * FROM "user" WHERE id = $1', [
         id,
       ]);
       if (result.rows.length === 0) {
@@ -26,10 +44,18 @@ class User {
     }
   }
 
+  /**
+   * Function that return a user from the database if found or null if not
+   * @param {string} email the email of the user to find
+   * @async
+   * @static
+   * @returns {User<object>} new instance of user or null
+   * @throws {Error} if the email didn't match any user in the database
+   */
   static async getByEmail(email) {
     try {
       const { rows } = await client.query(
-        'SELECT * FROM user WHERE email = $1',
+        'SELECT * FROM "user" WHERE email = $1',
         [email]
       );
       if (rows.length === 0) {
@@ -41,10 +67,16 @@ class User {
     }
   }
 
+  /**
+   * Function that create a new user in the database
+   * @async
+   * @returns {User<object>} new instance of user
+   * @throws {Error} if something went wrong during the query or if the user already exists
+   */
   async create() {
     try {
       const { rows } = await client.query(
-        'INSERT INTO user (firstname, lastname, email, password, role) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        'INSERT INTO "user" (firstname, lastname, email, password, role) VALUES ($1, $2, $3, $4, $5) RETURNING *',
         [this.firstname, this.lastname, this.email, this.password, this.role]
       );
       return new User(rows[0]);
@@ -53,10 +85,16 @@ class User {
     }
   }
 
+  /**
+   * Function that update the current instance of user in the database
+   * @async
+   * @returns {User<object>} new instance of user
+   * @throws {Error} if something went wrong during the query or if the user doesn't exist
+   */
   async update() {
     try {
       const { rows } = await client.query(
-        'UPDATE user SET firstname = $1, lastname = $2, password = $3, role = $4 WHERE id = $5 RETURNING *',
+        'UPDATE "user" SET firstname = $1, lastname = $2, password = $3, role = $4 WHERE id = $5 RETURNING *',
         [this.firstname, this.lastname, this.password, this.role, this.id]
       );
       return new User(rows[0]);
@@ -67,7 +105,7 @@ class User {
 
   async delete() {
     try {
-      await client.query('DELETE FROM user WHERE id = $1', [this.id]);
+      await client.query('DELETE FROM "user" WHERE id = $1', [this.id]);
       return true;
     } catch (err) {
       throw new Error(err);
