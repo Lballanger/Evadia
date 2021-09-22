85e88acb302edb8fd14b7559bbac9cdc6fc5e55a
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-22 16:19:48 +0200

    🔨 Add logout & refreshToken methods in authController

--- back/app/controllers/authController.js
+++ back/app/controllers/authController.js
@@ -50,6 +50,24 @@ const authController = {
       return response.status(500).json(error.message);
     }
   },
+  logout: async (request, response) =>
+    // TODO: remove token from db or redis
+    // TODO: find a way to revoke token
+    response.json('Logged out'),
+  refreshToken: async (request, response) => {
+    const { token } = request.body;
+    try {
+      if (token == null) return response.status(401).json('No token provided');
+      // TODO: get refreshToken from db or redis and check if exists
+      const decoded = jwtService.validateToken(token, true);
+      if (decoded == null) return response.status(401).json('Invalid token');
+      const newToken = await jwtService.sign({ id: decoded.id });
+      // TODO: Store the new token in db or redis
+      return response.json({ token: newToken });
+    } catch (error) {
+      return response.status(500).json(error.message);
+    }
+  },
 };
 
 module.exports = authController;
