9f061fa00217004e45b92f9fcf40c840531733d3
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-22 13:42:10 +0200

    ✨ Create User model

---
+++ back/app/models/user.js
@@ -0,0 +1,14 @@
+class User {
+  /**
+   * The User class
+   * @param {object} obj a literal object with the following properties: id?, firstname, lastname, email, password, role
+   */
+  constructor(obj = {}) {
+    // eslint-disable-next-line guard-for-in
+    for (const propName in obj) {
+      this[propName] = obj[propName];
+    }
+  }
+}
+
+module.exports = User;
