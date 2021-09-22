6db05ffb0a75192c2a8756b03252309f636426f3
Lballanger <ballanger.loic@gmail.com>
2021-09-22 10:32:02 +0200

    Deletion of the locality and commune_has_postal_code

--- back/deploy/init.sql
+++ back/deploy/init.sql
@@ -5,21 +5,12 @@ BEGIN;
 CREATE TABLE commune (
     code_insee INT PRIMARY KEY,
     code_departement INT,
+    code_postal INT[],
     code_region INT,
     city_name TEXT,
     longitude FLOAT,
     latitude FLOAT,
     population INT
-);
-
-CREATE TABLE locality (
-    zip_code INT PRIMARY KEY
-);
-
-CREATE TABLE commune_has_postal_code (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    commune_code_insee INT NOT NULL REFERENCES commune(code_insee) ON DELETE CASCADE,
-    locality_zip_code INT NOT NULL REFERENCES locality(zip_code) ON DELETE CASCADE
 );
 
 COMMIT;
--- back/verify/init.sql
+++ back/verify/init.sql
@@ -3,7 +3,5 @@
 BEGIN;
 
 SELECT * FROM commune WHERE false;
-SELECT * FROM locality WHERE false;
-SELECT * FROM commune_has_postal_code WHERE false;
 
 ROLLBACK;
