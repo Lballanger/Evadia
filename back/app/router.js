67db1a69e994ec18546fd3feb1887cb94989a34a
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-22 19:22:33 +0200

    ✨ Add auth routes

--- back/app/router.js
+++ back/app/router.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 
 const searchController = require('./controllers/searchController');
+const authController = require('./controllers/authController');
+const { authMiddleware } = require('./middlewares/authMiddleware');
 
 const communeSchema = require('./schemas/commune');
 const { validateQuery } = require('./services/validator');
@@ -37,6 +39,36 @@ router.post('/api/search/criteria', searchController.findByCriteria);
 
 // /city
 
-// /auth
+/**
+ * Get data for the current connected user
+ * @route GET /user
+ * @group Auth
+ * @summary Return data for the current connected user or error message if not connected with valid token
+ */
+router.get('/api/user', authMiddleware, authController.user);
+
+/**
+ * Log user in
+ * @route POST /auth/login
+ * @group Auth
+ * @summary Return user data and token if user successfully connected or error message
+ */
+router.post('/api/auth/login', authController.login);
+
+/**
+ * Sign user in
+ * @route POST /auth/register
+ * @group Auth
+ * @summary Return user data and token if user successfully created and if email not already store in the database otherwise return an error message
+ */
+router.post('/api/auth/register', authController.register);
+
+/**
+ * Sign user out
+ * @route POST /auth/logout
+ * @group Auth
+ * @summary Return error message if user was not connected (no token provided or invalid) otherwise return status 204
+ */
+router.post('/api/auth/logout', authMiddleware, authController.logout);
 
 module.exports = router;
