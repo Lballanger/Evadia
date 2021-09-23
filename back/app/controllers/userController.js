7d3a09b60dfbe33d73482ac31955fd573d57c558
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-23 14:19:06 +0200

    🔨 Add "update" method to userController

--- back/app/controllers/userController.js
+++ back/app/controllers/userController.js
@@ -17,6 +17,24 @@ const userController = {
       return response.status(500).json(error.message);
     }
   },
+  update: async (request, response) => {
+    const { id } = request.user;
+    const { firstname, lastname, email, password, role } = request.body;
+    try {
+      const user = await User.getById(id);
+      if (!user) return response.status(404).json('User not found');
+      const updatedUser = await User.update(id, {
+        firstname,
+        lastname,
+        email,
+        password,
+        role,
+      });
+      return response.json(updatedUser);
+    } catch (error) {
+      return response.status(500).json(error.message);
+    }
+  },
 };
 
 module.exports = userController;
