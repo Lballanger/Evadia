32b9574d6edf133233c69b2c618e4eeb63a8bba9
François Charron <fcharron.dev@gmail.com>
2021-09-23 13:51:18 +0200

    install axios

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension';
-
 import reducer from '../reducers';
 
 const store = createStore(reducer, devToolsEnhancer());
