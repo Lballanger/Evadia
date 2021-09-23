e7da47b09f0a78499ae45a8cd5a7079a7129f074
Clement Kieken <ckieken@protonmail.com>
2021-09-23 19:22:03 +0200

    404 done

--- front/src/components/Notfound/index.jsx
+++ front/src/components/Notfound/index.jsx
@@ -1,1 +1,21 @@
-// trucquisertarien
+import React from 'react';
+import Footer from '../Footer';
+import Header from '../Header';
+
+import './styles.scss';
+import gif from '../../../public/404.gif';
+
+const Notfound = () => (
+  <>
+    <Header />
+    <div className="notfound__container">
+      <img src={gif} alt="404" />
+      <p className="notfound__container__p">
+        Choisi une commune présente dans notre BDD wsh !
+      </p>
+    </div>
+    <Footer />
+  </>
+);
+
+export default Notfound;
