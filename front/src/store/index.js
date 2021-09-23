adfd7366edba01a1032f06a8d8da9a9bdc38028c
François Charron <fcharron.dev@gmail.com>
2021-09-23 11:55:06 +0200

    setup redux react-redux devtools and reducers store folders

---
+++ front/src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import reducer from '../reducers';
+
+const store = createStore(reducer, devToolsEnhancer());
+
+export default store;
