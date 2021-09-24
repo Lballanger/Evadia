a6bbdd227e0b291d51b4232135a6ae768bbfb0fd
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-24 10:46:37 +0200

    🔧 Add "ssl" configuration for production

--- back/app/database.js
+++ back/app/database.js
@@ -1,7 +1,14 @@
 const { Pool } = require('pg');
 
+const ssl = {};
+
+if (process.env.NODE_ENV === 'production') {
+  ssl.rejectUnauthorized = false;
+}
+
 const config = {
   connectionString: process.env.DATABASE_URL,
+  ssl,
 };
 
 const pool = new Pool(config);
