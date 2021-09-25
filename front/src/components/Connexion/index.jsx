c61f614beed288112bdb6df2605526753ba87f2a
GermainMichaud <gmichaud.dev@gmail.com>
2021-09-26 01:00:24 +0200

    :lipstick: Update login page

--- front/src/components/Connexion/index.jsx
+++ front/src/components/Connexion/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Header from '../Header';
 import Footer from '../Footer';
 
@@ -8,6 +8,7 @@ import Form from '../Shared/Form';
 import Input from '../Shared/Input';
 import API from '../../api';
 import userStore from '../../store/user';
+import useWindowSize from '../../hooks/useWindowSize';
 
 const initialInputs = {
   email: '',
@@ -16,16 +17,17 @@ const initialInputs = {
 
 const Connexion = () => {
   const [inputs, setInputs] = useState({ ...initialInputs });
-  const setUser = userStore((state) => state.setUser);
+  const setUser = userStore(state => state.setUser);
+  const { isMobile } = useWindowSize();
 
-  const inputChange = (event) => {
-    setInputs((state) => ({
+  const inputChange = event => {
+    setInputs(state => ({
       ...state,
       [event.target.name]: event.target.value,
     }));
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async event => {
     event.preventDefault();
     try {
       const data = await API.doLogin(inputs);
@@ -41,23 +43,47 @@ const Connexion = () => {
       <Header />
       <div className="connexion">
         <div className="connexion__title">CONNEXION</div>
-        <Form onSubmit={handleSubmit}>
-          <Input
-            type="email"
-            labelText="Email"
-            id="email"
-            value={inputs.email}
-            onChange={inputChange}
-          />
-          <Input
-            type="password"
-            labelText="Mot de passe"
-            id="password"
-            value={inputs.password}
-            onChange={inputChange}
-          />
-          <button type="submit">Se connecter</button>
-        </Form>
+        <section className="connexion__section">
+          {!isMobile && (
+            <div>
+              <img src="" alt="img-connexion" />
+              <span>
+                <h3>Connectez-vous</h3>
+                <p>
+                  Connectez-vous pour accéder à votre compte, ou{' '}
+                  <NavLink to="/inscription">créez un compte</NavLink>
+                </p>
+              </span>
+            </div>
+          )}
+          <Form onSubmit={handleSubmit} className="connexion__form">
+            <Input
+              type="email"
+              labelText="Email"
+              id="email"
+              value={inputs.email}
+              onChange={inputChange}
+              className="connexion__form-group"
+            />
+            <Input
+              type="password"
+              labelText="Mot de passe"
+              id="password"
+              value={inputs.password}
+              onChange={inputChange}
+              className="connexion__form-group"
+            />
+            <button type="submit" className="connexion__form-group-button">
+              Se connecter
+            </button>
+            <Link
+              to="/forgottenPassword"
+              className="connexion__form__forgottenPassword"
+            >
+              Mot de passe oublié
+            </Link>
+          </Form>
+        </section>
         {/* <form className="connexion__form">
         <div className="connexion__form__email">
           <p>Email</p>
