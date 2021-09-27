5cba39c3e9abbcae6711c5957e104e4ee2197593
Clement Kieken <ckieken@protonmail.com>
2021-09-27 17:42:51 +0200

    upgrade map

--- front/src/components/Map/index.jsx
+++ front/src/components/Map/index.jsx
@@ -1,38 +1,40 @@
 /* eslint-disable no-lone-blocks */
 /* eslint-disable react/react-in-jsx-scope */
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  ZoomControl,
+} from 'react-leaflet';
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './styles.scss';
 
-const Map = () => {
+const Map = ({ cities }) => {
   const position = [48.862725, 2.287592];
   return (
     <div>
-      <MapContainer
-        center={position}
-        zoom={10}
-        className="map__container"
-        scrollWheelZoom
-      >
+      <MapContainer center={position} zoom={10} className="map__container">
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>Nom de la ville.</Popup>
-        </Marker>
+        <ZoomControl position="bottomright" />
+        {cities.map((city) => (
+          <Marker position={city.coords}>
+            <Popup>{city.city_name}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
 };
-
+Map.propTypes = PropTypes.shape({
+  cities: PropTypes.arrayOf({
+    city_name: PropTypes.string,
+    population: PropTypes.string,
+    coords: PropTypes.arrayOf(PropTypes.number),
+  }),
+}).isRequired;
 export default Map;
-
-{
-  /* <MapContainer center={start_coordinate} zoom={10} scrollWheelZoom={false}>
-<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-<Marker position={start_coordinate} icon={positionIcon} />
-<Marker position={end_coordinate} icon={positionIcon} />
-</MapContainer> */
-}
--- front/src/components/Results/index.jsx
+++ front/src/components/Results/index.jsx
@@ -1,85 +1,65 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import useWindowSize from '../../hooks/useWindowSize';
+import cityStore from '../../store/city';
 import Map from '../Map';
 
 import './styles.scss';
 
-const Results = () => (
-  <>
-    <Map />
-    <div className="list__results">
-      <div className="results">
-        <div className="results__criteria">
-          <div className="criteria__return">
-            <NavLink
-              type="button"
-              to="/criteria"
-              className="criteria__return__btn"
-            >
-              Modifier critères
-            </NavLink>
+const Results = () => {
+  const { isMobile } = useWindowSize();
+  const cities = cityStore((state) => state.cities);
+
+  const transformCoords = (data) =>
+    data.map((city) => ({
+      city_name: city.city_name,
+      population: city.population,
+      coords: [city.coordinates.x, city.coordinates.y],
+    }));
+
+  return (
+    <>
+      {!isMobile && <Map cities={transformCoords(cities)} />}
+      <div className={`list__results${isMobile ? '' : ' with-map'}`}>
+        <div className="results">
+          <div className="results__criteria">
+            <div className="criteria__return">
+              <NavLink
+                type="button"
+                to="/criteria"
+                className="criteria__return__btn"
+              >
+                Modifier critères
+              </NavLink>
+            </div>
+            <div className="resultsNbr">
+              {cities.length}
+              ville{cities.length > 1 ? 's' : ''} dans votre recherche(s).
+            </div>
           </div>
-          <div className="resultsNbr">0 villes dans votre recherche(s).</div>
         </div>
-      </div>
 
-      <div className="result">
-        <button type="button" className="result__button">
-          <img
-            className="picture"
-            src="../../assets/images/paris.jpg"
-            alt="city"
-          />
-          <div className="result__info">
-            <p>Paris</p>
-            <div className="infos">
-              <p>info</p>
-              <p>info</p>
-            </div>
+        {cities.map((city) => (
+          <div className="result">
+            <button type="button" className="result__button">
+              <img
+                className="picture"
+                src={`../../assets/images/${city.city_name}.jpg`}
+                alt="city"
+              />
+              <div className="result__info">
+                <p>{city.city_name}</p>
+                <div className="infos">
+                  <p>{city.population}</p>
+                  <p>info</p>
+                </div>
+              </div>
+            </button>
           </div>
-        </button>
+        ))}
       </div>
-
-      <div className="result">
-        <button type="button" className="result__button">
-          <img className="picture" src="../public/paris.jpg" alt="city" />
-          <div className="result__info">
-            <p>Paris</p>
-            <div className="infos">
-              <p>info</p>
-              <p>info</p>
-            </div>
-          </div>
-        </button>
-      </div>
-
-      <div className="result">
-        <button type="button" className="result__button">
-          <img className="picture" src="../public/paris.jpg" alt="city" />
-          <div className="result__info">
-            <p>Paris</p>
-            <div className="infos">
-              <p>info</p>
-              <p>info</p>
-            </div>
-          </div>
-        </button>
-      </div>
-
-      <div className="result">
-        <button type="button" className="result__button">
-          <img className="picture" src="../public/paris.jpg" alt="city" />
-          <div className="result__info">
-            <p>Paris</p>
-            <div className="infos">
-              <p>info</p>
-              <p>info</p>
-            </div>
-          </div>
-        </button>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Results;
