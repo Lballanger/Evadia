b3b0a40aeb51ef67f258cd32fecfb8ef3894aee0
Clement Kieken <ckieken@protonmail.com>
2021-09-27 15:35:27 +0200

    css mobile & desktop criteria page

--- front/src/components/Footer/index.jsx
+++ front/src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ import './styles.scss';
 const Footer = () => (
   <footer className="footer">
     <p className="footer__p">©Copyright Evadia 2021. Tous droits réservés.</p>
-
+    <p className="footer__mobile">©Evadia 2021.</p>
     <div className="footer__display">
       <NavLink className="footer__button" type="button" to="/contact">
         Contact
