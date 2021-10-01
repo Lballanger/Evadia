46d1b0ffbcd552e25b1ef274e3d95c5e103c9328
Kalison <alison.serra@hotmail.fr>
2021-10-01 16:26:59 +0200

    🔨  Add the get_internet function

--- back/migrations/deploy/internet_table.sql
+++ back/migrations/deploy/internet_table.sql
@@ -8,4 +8,22 @@ CREATE TABLE private.internet (
 	coverage INT
 );
 
+CREATE OR REPLACE FUNCTION private.get_internet(code TEXT) RETURNS JSON AS $$
+SELECT
+  json_agg(
+    json_build_object(
+      'id',
+      internet.id,
+      'commune_code',
+      internet.commune_code,
+      'coverage',
+      internet.coverage
+    )
+  )
+FROM
+  private.internet AS internet
+WHERE
+  internet.commune_code = code;
+$$ LANGUAGE SQL STRICT;
+
 COMMIT;
