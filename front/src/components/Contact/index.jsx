b0e1f79307a253aaee06a0b34d8d8ff08b750997
François Charron <fcharron.dev@gmail.com>
2021-10-04 11:33:08 +0200

    fixed error

--- front/src/components/Contact/index.jsx
+++ front/src/components/Contact/index.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-console */
 import React, { useState } from 'react';
+import API from '../../api';
 import Form from '../Shared/Form';
 import Input from '../Shared/Input';
 import Textarea from '../Shared/Textarea';
@@ -22,19 +24,34 @@ const Contact = () => {
     }));
   };
 
-  const handleSubmit = (event) => {
+  const clearInput = (id) => {
+    setInputs((state) => ({
+      ...state,
+      [id]: '',
+    }));
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     // TODO: Add logic - Send data to api
+    try {
+      const data = await API.doContact(inputs);
+      setInputs({ ...initialInputs });
+      console.log(data);
+    } catch (err) {
+      console.log('error contact', err);
+    }
   };
 
   return (
     <div className="contact">
       <div className="inscription__title">CONTACT</div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Input
           id="name"
           value={inputs.name}
           onChange={handleChange}
+          onClear={clearInput}
           labelText="Votre nom"
           placeholder="Entrez votre nom"
           type="text"
@@ -43,6 +60,7 @@ const Contact = () => {
           id="email"
           value={inputs.email}
           onChange={handleChange}
+          onClear={clearInput}
           labelText="Votre email"
           placeholder="ex: mon@email.com"
           type="email"
@@ -51,6 +69,7 @@ const Contact = () => {
           id="message"
           value={inputs.message}
           onChange={handleChange}
+          onClear={clearInput}
           labelText="Votre message"
         />
         <button type="submit" className="contact__form-group-button">
