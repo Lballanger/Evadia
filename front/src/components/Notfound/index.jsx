1b5a068feff613cd35576e896e64d97e2bec173b
François Charron <fcharron.dev@gmail.com>
2021-10-15 10:04:30 +0200

    add button notfound

--- front/src/components/Notfound/index.jsx
+++ front/src/components/Notfound/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './styles.scss';
 // import island from '../../assets/images/island.png';
@@ -8,6 +9,11 @@ const Notfound = () => (
   <div className="notfound__container">
     <img className="notfound__img" src={NF} alt="404" />
     <div className="font-link" />
+    <NavLink type="button" to="/">
+      <button type="submit" className="notFound__form-group-button">
+        Retour à la page d&apos;accueil
+      </button>
+    </NavLink>
   </div>
 );
 
